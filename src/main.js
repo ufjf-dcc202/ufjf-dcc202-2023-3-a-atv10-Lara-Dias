4abc1d01aa8004cde6df55153fe58848d131704b
Lara Dias <lara.dias@estudante.ufjf.br>
2023-11-22 20:44:37 -0300

    adiciona função na main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { getMaria, getJoao} from "./joaoEMaria.js"
+import { getMaria, getJoao, deJoaoParaMaria} from "./joaoEMaria.js"
 
 console.log("MAIN");
 let maria = getMaria();
 let joao = getJoao();
 console.log(maria,joao);
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
+console.log(maria, joao);
